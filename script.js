f8c29c081a76a98ec4d7d293b6c424db0a460a51
knlwgl <knlwgl@gmail.com>
2023-12-03 23:18:11 -0500

    Added button clicks

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 
 let signinBtn = document.getElementById("signinBtn");
 let registerBtn = document.getElementById("registerBtn");
+let emailField = document.getElementById("email");
+let title = document.getElementById("title");
+
+signinBtn.onclick = function() {
+    signinBtn.classList.remove("disable");
+    registerBtn.classList.add("disable");
+    emailField.style.maxHeight = "0px";
+    title.innerHTML = "Build Base";
+}
+
+registerBtn.onclick = function() {
+    emailField.style.maxHeight = "65px";
+    title.innerHTML = "Register";
+    registerBtn.classList.remove("disable");
+    signinBtn.classList.add("disable");
+}
